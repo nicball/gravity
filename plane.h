9fbb7ea20f4531844b29c0b6d09b06092e1f8a52
Nick Ballard <znhihgiasy@gmail.com>
2015-12-31 22:32:12 +0800

    Update and rename plane.hpp to plane.h

--- plane.h
+++ plane.h
@@ -1,15 +1,16 @@
-#ifndef PLANE_HPP_
-#define PLANE_HPP_
+#ifndef PLANE_H
+#define PLANE_H
+#pragma once
 
+#include "vector3d.h"
+#include "world.h"
 #include <random>
-#include "particle.hpp"
 
 struct plane {
-    static constexpr double PI  = particle::PI;
-    static constexpr double K   = 40;
+    static constexpr double K = 500;
     static constexpr double ETA = 0.3;
-    static constexpr double MU  = 0.25 * PI;
-    static constexpr double D   = 50;
+    static constexpr double MU = 0.25 * PI;
+    static constexpr double D = 50;
 
     vector3d normal;
     double C;
@@ -24,8 +25,8 @@ struct plane {
         return frand()>0.5 ? 1 : -1;
     }
 
-    void act_on(particle::list& arr) const;
-    void operator () (particle::list& arr) const { act_on(arr); }
+    void act_on(world& arr) const;
+    void operator () (world& arr) const { act_on(arr); }
 };
 
 #endif
