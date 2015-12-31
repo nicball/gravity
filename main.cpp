9140f060c7da82e8a170e1289bddf414855e8bb9
Nick Ballard <znhihgiasy@gmail.com>
2015-12-31 22:30:06 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 
-#include "particle.hpp"
-#include "plane.hpp"
-#include "gravity.hpp"
+#include "world.h"
+#include "plane.h"
 
-particle::list world;
+class world world(10000);
 double view_factor = 1.0 / 1600.0;
 
 void init() {
@@ -19,57 +18,63 @@ void init() {
 
         //world[i].position.x = (i%30-15) * 10;
         //world[i].position.y = i/30*10 + 300;
-        
-        world[i].position.y = i+1400;
-        world[i].position.x = 400;
+
+        world[i].position.y = i + 1400;
+        world[i].position.x = 0;
 
         //world[i].position.y = i+2000;
         //world[i].position.x = i+200;
     }
-    world.clear_npc()
-         .add_npc(gravity)
-         .add_npc(plane{{0, 1, 0},  400})
-         .add_npc(plane{{0, 0, 1},  -100})
-         .add_npc(plane{{0, 0, 1},  100})
-         .add_npc(plane{{1, 1, 0}, 0})
-         .add_npc(plane{{1, -1, 0}, 0});
+    world.add_npc(gravity)
+        //.add_npc(interact)
+        //.add_npc(plane{{0, 1, 0}, 400})
+        //.add_npc(plane{{0, 0, 1}, -100})
+        //.add_npc(plane{{0, 0, 1}, 100})
+        //.add_npc(plane{{1, 1, 0}, 0})
+        //.add_npc(plane{{1, -1, 0}, 0});
+
+        .add_npc(plane{{0, 1, 0}, 0})
+        .add_npc(plane{{1, 0, 0}, -100})
+        .add_npc(plane{{-1, 0, 0}, -100})
+        .add_npc(plane{{0, 0, 1}, -100})
+        .add_npc(plane{{0, 0, -1}, -100});
 }
 
 void render() {
     world.step();
 
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0, 0, 0.7);
+    glColor3f(0.0f, 0.0f, 0.7f);
     glBegin(GL_POINTS);
     for (auto& i : world)
         glVertex3d(i.position.x * view_factor, i.position.y * view_factor, i.position.z * view_factor);
     glEnd();
 
     glColor3f(1, 1, 1);
-    //glBegin(GL_LINES);
-    //glVertex3d(-1, -400 * view_factor, 0);
-    //glVertex3d(1, -400 * view_factor, 0);
-    //glEnd();
-
-    //glBegin(GL_LINES);
-    //glVertex3d(100 * view_factor, 1, 0);
-    //glVertex3d(100 * view_factor, -1, 0);
-    //glEnd();
-
-    //glBegin(GL_LINES);
-    //glVertex3d(-100 * view_factor, 1, 0);
-    //glVertex3d(-100 * view_factor, -1, 0);
-    //glEnd();
-
     glBegin(GL_LINES);
-    glVertex3d(-1, 1, 0);
-    glVertex3d(1, -1, 0);
+    glVertex3d(-1, 0, 0);
+    glVertex3d(1, 0, 0);
     glEnd();
 
     glBegin(GL_LINES);
-    glVertex3d(-1, -1, 0);
-    glVertex3d(1, 1, 0);
+    glVertex3d(100 * view_factor, 1, 0);
+    glVertex3d(100 * view_factor, -1, 0);
     glEnd();
+
+    glBegin(GL_LINES);
+    glVertex3d(-100 * view_factor, 1, 0);
+    glVertex3d(-100 * view_factor, -1, 0);
+    glEnd();
+
+    //glBegin(GL_LINES);
+    //glVertex3d(-1, 1, 0);
+    //glVertex3d(1, -1, 0);
+    //glEnd();
+
+    //glBegin(GL_LINES);
+    //glVertex3d(-1, -1, 0);
+    //glVertex3d(1, 1, 0);
+    //glEnd();
 
     glFlush();
     glutSwapBuffers();
@@ -85,12 +90,11 @@ void on_mouse(int button, int state, int x, int y) {
 }
 
 int main(int argc, char** argv) {
-    srand(time(0));
     init();
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
-    glutInitWindowPosition(50, 50);
-    glutInitWindowSize(1440, 900);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(1280, 720);
     glutCreateWindow("");
     glutDisplayFunc(render);
     glutIdleFunc(render);
