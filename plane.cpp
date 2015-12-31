909a41ff2f6b15f821f455f41e71783bace891e7
Nick Ballard <znhihgiasy@gmail.com>
2015-12-31 22:31:56 +0800

    Update plane.cpp

--- plane.cpp
+++ plane.cpp
@@ -1,27 +1,24 @@
-#include "plane.hpp"
+#include "plane.h"
 
-void plane::act_on(particle::list& arr) const {
-    for (auto& i : arr) {
-        auto norm = normal;
-        auto lam = (norm*i.position + C) / (norm*norm);
-        norm *= lam;
-        double d = norm.length();
+void plane::act_on(world& w) const {
+    for (auto& i : w) {
+        auto pos = normal * (normal*i.position + C) / (normal*normal);
+        double d = pos.length();
         if (d < D) {
-            auto r = std::tan(frand()*MU) * norm.length();
-            auto ytoz = std::atan2(norm.y, norm.z);
-            auto ztox = std::atan2(norm.z, norm.x) - 0.5*PI;
+            auto r = std::tan(frand()*MU) * pos.length();
+            auto ytoz = std::atan2(pos.z, pos.y);
+            auto xtoz = std::atan2(pos.z, pos.x);
             vector3d dn;
             dn.x = rs() * frand() * r;
             dn.y = rs() * std::sqrt(r*r - dn.x*dn.x);
             dn.z = 0;
             dn.rotate_x(-ytoz);
-            dn.rotate_y(ztox);
-            norm += dn;
-            norm.set_length(K * (D-d));
-            if (norm*i.velocity / (norm.length()*i.velocity.length()) > 0)
-                norm *= ETA;
-            i.acceleration += norm;
+            dn.rotate_y(-xtoz);
+            auto a = pos + dn;
+            a.set_length(K * (D - d));
+            if (a*i.velocity / (a.length()*i.velocity.length()) > 0)
+                a *= ETA;
+            i.acceleration += a;
         }
     }
 }
-
