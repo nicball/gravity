6f93611df17373d855da1ec326f68e824892b636
Nick Ballard <znhihgiasy@gmail.com>
2016-01-02 16:33:47 +0800

    Update world.cpp

--- world.cpp
+++ world.cpp
@@ -1,13 +1,17 @@
 #include "world.h"
 
 void interact(world& w) {
+    static constexpr double R = 0.2;
+    static constexpr double K = 0.5;
+    static constexpr double E = 0.001;
     for (auto& i : w) {
         for (auto& j : w) {
             if (&i != &j) {
                 auto a = j.position - i.position;
                 auto d = a.length();
-                if (d < world::Dmin || d > 100 * world::Dmin) continue;
-                a.set_length(world::K / (d*d*d) - world::K / (d*d));
+                auto f = E*i.mass*j.mass * (1 / (d*d) - 1 / (d*d*d));
+                if (d < R) f += (R - d) * K;
+                a.set_length(f);
                 i.acceleration += a;
             }
         }
